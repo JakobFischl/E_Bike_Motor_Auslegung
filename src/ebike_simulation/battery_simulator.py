e2771d5d451e4ac2c0df1b38899751dc96fe5b3d
Jakob Fischl <fj5430@mci4me.at>
2026-07-07 22:00:55 +0200

    feat(simulator): track truncated profiles and adjust empty/full handling
    Truncate duration and current profile if the battery capacity isn't sufficient at the interval, where the battery died.

--- src/ebike_simulation/battery_simulator.py
+++ src/ebike_simulation/battery_simulator.py
@@ -13,37 +13,48 @@ class BatterySimulator:
     def __init__(self, battery_pack: BatteryPack) -> None:
         self.battery_pack = battery_pack
         self.voltage_profile = []
+        self.truncated_duration_profile = []
+        self.truncated_current_profile = []
 
     def simulate(self, current_profile: list[float], duration_profile: list[float]) -> None:
         
         self.voltage_profile = [self.battery_pack.voltage()]
 
-        for current, duration in zip(current_profile, duration_profile):
+        for i, (current, duration) in enumerate(zip(current_profile, duration_profile)):
             
-            if self.battery_pack.is_empty() == True and current > 0:
+            if self.battery_pack.is_empty():
                 print("Battery is empty: cannot discharge further!")
+                self.truncated_duration_profile = duration_profile[:i]
+                self.truncated_current_profile = current_profile[:i]
                 break
-            elif self.battery_pack.is_full() == True and current < 0:
+                
+            elif self.battery_pack.is_full() and current < 0:
                 print("Battery is full: energy must be dissipated!")
-                continue
+                v = self.battery_pack.voltage()
+                self.voltage_profile.append(v)
+                self.truncated_duration_profile = duration_profile
+                self.truncated_current_profile = current_profile
             else:
                 self.battery_pack.apply_current(current, duration)
                 v = self.battery_pack.voltage(current)
                 self.voltage_profile.append(v)
+                self.truncated_duration_profile = duration_profile
+                self.truncated_current_profile = current_profile
 
 
 if __name__ == "__main__":
     load_current = [3.0, 11.0, 4.0, -1.5, 1.0]
     load_durations = [300.0, 240.0, 90.0, 150.0, 120.0]
 
-    plot_current_profile(current_profile=load_current, duration_profile=load_durations)
-
-    battery = BatteryPack(capacity_nom_Ah=10, initial_soc=0.01, Vmin=32.0, Vmax=42.0)
+    battery = BatteryPack(capacity_nom_Ah=10, initial_soc=0.10833334, Vmin=32.0, Vmax=42.0)
+    print(battery)
     bat_sim = BatterySimulator(battery)
     bat_sim.simulate(load_current, load_durations)
     print(battery)
 
-    plot_voltage_profile(voltage_profile=bat_sim.voltage_profile, duration_profile=load_durations)
-    plot_voltage_and_current_profile(bat_sim.voltage_profile, load_current, load_durations)
+    plot_current_profile(current_profile=bat_sim.truncated_current_profile, duration_profile=bat_sim.truncated_duration_profile)
+
+    plot_voltage_profile(voltage_profile=bat_sim.voltage_profile, duration_profile=bat_sim.truncated_duration_profile)
+    plot_voltage_and_current_profile(bat_sim.voltage_profile, bat_sim.truncated_current_profile, bat_sim.truncated_duration_profile)
 
     input("Press Enter to continue...")
