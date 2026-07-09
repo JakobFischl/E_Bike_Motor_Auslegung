5460adec07389891af751e857d31edc0322f52db
Jakob Fischl <fj5430@mci4me.at>
2026-07-09 23:10:51 +0200

    refactor(sizer): add type hints, extract min_soc helper, simplify logic
    Create nested function get_min_soc to remove duplicate code. Add battery_class type hint. Simplify while loops and if clauses. Put test code behind if name == main clause.

--- src/ebike_simulation/battery_sizer.py
+++ src/ebike_simulation/battery_sizer.py
@@ -1,55 +1,46 @@
 from battery_simulator import BatterySimulator
-from nmc_battery import NMCBatteryPack
-from lipo_battery import LiPoBatteryPack
+from battery_pack import BatteryPack
 import math
 
 
-def determine_capacity(battery_class, current_profile: list[float], duration_profile: list[float], soc_reserve: float, initial_soc: float = 1.0) -> float:
+def determine_capacity(battery_class: type[BatteryPack], current_profile: list[float], duration_profile: list[float], soc_reserve: float, initial_test_capacity: float = 1.0, initial_soc: float = 1.0) -> float:
 
-    trial_capacity_Ah = 0.1
-    battery = battery_class(initial_soc=initial_soc, capacity_nom_Ah=trial_capacity_Ah)
-    bat_sim = BatterySimulator(battery)
-    bat_sim.simulate(current_profile, duration_profile)  
-    min_soc = min(bat_sim.soc_profile)
-
-    if min_soc >= soc_reserve:
-        c_sufficient = True
-    else:
-        c_sufficient = False
-
-    while c_sufficient is not True:
-        trial_capacity_Ah = trial_capacity_Ah * 2
-        battery = battery_class(initial_soc=initial_soc, capacity_nom_Ah=trial_capacity_Ah)
+    def get_min_soc(capacity):
+        battery = battery_class(initial_soc=initial_soc, capacity_nom_Ah=capacity)
         bat_sim = BatterySimulator(battery)
         bat_sim.simulate(current_profile, duration_profile)  
         min_soc = min(bat_sim.soc_profile)
+        return min_soc
+
+
+    trial_capacity_Ah_high = initial_test_capacity
+    trial_capacity_Ah_low = trial_capacity_Ah_high / 2
+    min_soc_high = get_min_soc(trial_capacity_Ah_high)
+
+
+    while min_soc_high < soc_reserve:
+        trial_capacity_Ah_high = trial_capacity_Ah_high * 2
+        min_soc_high = get_min_soc(trial_capacity_Ah_high)
+    
+    low = trial_capacity_Ah_low
+    high = trial_capacity_Ah_high
+    while True:
+        mid = (low + high) / 2
+
+        min_soc = get_min_soc(mid)
+
         if min_soc >= soc_reserve:
-            c_sufficient = True
-        else:
-            c_sufficient = False
-    else:
-        low = trial_capacity_Ah / 2
-        high = trial_capacity_Ah
-        while low <= high:
-            mid = (low + high) / 2
-
-            battery = battery_class(initial_soc=initial_soc, capacity_nom_Ah=mid)
-            bat_sim = BatterySimulator(battery)
-            bat_sim.simulate(current_profile, duration_profile)  
-            min_soc = min(bat_sim.soc_profile)
-        
-            if min_soc >= soc_reserve and (high - low) <= 0.5:
-                sufficient_capacity = math.ceil(mid)
-                return sufficient_capacity
-            elif min_soc >= soc_reserve and (high - low) > 0.5:
-                high = mid
+            if (high - low) <= 0.5:
+                return math.ceil(mid*10) / 10
             else:
-                low = mid
+                high = mid
+        else:
+            low = mid
 
+if __name__ == "__main__":
+    
+    load_current = [15.5, 12.0, 13.0, 14.0, 10.0]
+    load_durations = [800.0, 240.0, 90.0, 150.0, 120.0]
 
-
-load_current = [15.5, 12.0, 13.0, 14.0, 10.0]
-load_durations = [800.0, 240.0, 90.0, 150.0, 120.0]
-
-sufficient_capacity = determine_capacity(NMCBatteryPack, current_profile=load_current, duration_profile=load_durations, soc_reserve=0.05)
-print(f"Battery has to have at least {sufficient_capacity} Ah")
+    sufficient_capacity = determine_capacity(BatteryPack, current_profile=load_current, duration_profile=load_durations, soc_reserve=0.05)
+    print(f"Battery has to have at least {sufficient_capacity} Ah.")
