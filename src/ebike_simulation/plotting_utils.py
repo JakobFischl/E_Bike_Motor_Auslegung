09d0ddc29fb33bfe9c238eefbed7a03bf671423c
Jakob Fischl <fj5430@mci4me.at>
2026-07-11 12:20:06 +0200

    docs(plotting): update docstrings and comments for methods

--- src/ebike_simulation/plotting_utils.py
+++ src/ebike_simulation/plotting_utils.py
@@ -5,10 +5,15 @@ import numpy as np
 # Set up local logger
 logger = logging.getLogger(__name__)
 
-def plot_current_profile(current_profile: np.ndarray, duration_profile: np.ndarray):
-    """Plots the current over time profile starting from t=0s."""
+def plot_current_profile(
+        current_profile: np.ndarray,
+        duration_profile: np.ndarray):
+    """
+    Plots the current over time profile starting from t=0s.
+    Raises a ValueError if the two arrays have different lengths.
+    Returns the figure.
+    """
     
-    # Error Handling & Logging
     if len(current_profile) != len(duration_profile):
         raise ValueError("Current and duration profiles must have the same length.")
     
@@ -26,10 +31,15 @@ def plot_current_profile(current_profile: np.ndarray, duration_profile: np.ndarr
     logger.debug("Current profile plot generated successfully.")
     return fig
 
-def plot_power_profile(power_profile: list[float], duration_profile: np.ndarray):
-    """Plots the power over time profile starting from t=0s."""
+def plot_power_profile(
+        power_profile: list[float],
+        duration_profile: np.ndarray):
+    """
+    Plots the power over time profile starting from t=0s.
+    Raises a ValueError if the two arrays have different lengths.
+    Returns the figure.
+    """
 
-    # Error Handling & Logging
     if len(power_profile) != len(duration_profile):
         raise ValueError("Power and duration profiles must have the same length.")
     
@@ -48,10 +58,15 @@ def plot_power_profile(power_profile: list[float], duration_profile: np.ndarray)
     return fig
 
 
-def plot_voltage_profile(voltage_profile: list[float], duration_profile: np.ndarray):
-    """Plots the voltage over time profile starting from t=0s."""
+def plot_voltage_profile(
+        voltage_profile: list[float],
+        duration_profile: np.ndarray):
+    """
+    Plots the voltage over time profile starting from t=0s.
+    Raises a ValueError if the two arrays have different lengths.
+    Returns the figure.
+    """
 
-    # Error Handling & Logging
     if len(voltage_profile) - 1 != len(duration_profile):
         raise ValueError("Voltage profile must be longer by 1 than duration profile.")
     
@@ -69,22 +84,35 @@ def plot_voltage_profile(voltage_profile: list[float], duration_profile: np.ndar
     logger.debug("Voltage profile plot generated successfully.")
     return fig
 
-def plot_voltage_and_current_profile(voltage_profile: list[float], current_profile: np.ndarray, duration_profile: np.ndarray):
-    """Plots the voltage and current over time profiles starting from t=0s."""
+def plot_voltage_and_current_profile(
+        voltage_profile: list[float],
+        current_profile: np.ndarray,
+        duration_profile: np.ndarray):
+    """
+    Plots the voltage and current over time profiles starting from t=0s.
+    Raises a ValueError if current and duration profiles don't have the
+    same length and are not one value shorter than voltage profile.
+    Returns the figure.
+    """
 
-    # Error Handling & Logging
     if not (len(voltage_profile) - 1 == len(current_profile) == len(duration_profile)):
-        raise ValueError("Current and duration profiles must have the same length, and voltage profile must be longer by 1.")
+        raise ValueError(
+            "Current and duration profiles must have the same length, "
+            "and voltage profile must be longer by 1.")
     
     logger.debug("Generating combined voltage and current profile plot...")
 
     edges = np.concatenate(([0.0], np.cumsum(duration_profile)))
 
-    fig, axV = plt.subplots(figsize=(9, 4.5))
+    fig, axV = plt.subplots()
     axI = axV.twinx()
 
-    axV.stairs(voltage_profile[1:], edges, baseline=None, linewidth=1.5, zorder=2.5, color="b", linestyle="-", label="Voltage U / V")
-    axI.stairs(current_profile, edges, baseline=None, linewidth=1.5, zorder=2.5, color="r", linestyle="--", label="Current I / A")
+    axV.stairs(
+        voltage_profile[1:], edges, baseline=None, linewidth=1.5,
+        zorder=2.5, color="b", linestyle="-", label="Voltage U / V")
+    axI.stairs(
+        current_profile, edges, baseline=None, linewidth=1.5,
+        zorder=2.5, color="r", linestyle="--", label="Current I / A")
     axV.set_xlabel("Time $t$ / s")
     axV.set_ylabel("Voltage $U$ / V", color="b")
     axI.set_ylabel("Current $I$ / A", color="r")
@@ -97,8 +125,14 @@ def plot_voltage_and_current_profile(voltage_profile: list[float], current_profi
     return fig
 
 
-def plot_soc_profile(soc_profile: list[float], duration_profile: np.ndarray):
-    """Plots the State of Charge over time starting at t = 0s"""
+def plot_soc_profile(
+        soc_profile: list[float],
+        duration_profile: np.ndarray):
+    """
+    Plots the State of Charge over time profile starting at t=0s.
+    Raises a ValueError if the SoC profile is not 1 value longer than the duration profile.
+    Returns the figure.
+    """
     
     if len(soc_profile) - 1 != len(duration_profile):
         raise ValueError("SoC profile must be longer by 1 than duration profile.")
