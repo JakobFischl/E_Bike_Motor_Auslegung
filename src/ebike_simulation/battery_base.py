445a0402bad6ab41538d98ce4ffc6a1a6a66b68d
Jakob Fischl <fj5430@mci4me.at>
2026-07-11 15:13:19 +0200

    fix(battery): fix BatteryBase to resemble current battery pack structure

--- src/ebike_simulation/battery_base.py
+++ src/ebike_simulation/battery_base.py
@@ -2,18 +2,31 @@ from abc import ABC, abstractmethod
 
 
 class BatteryBase(ABC):
-    @abstractmethod
-    def __init__(self, capacity_nom_Ah: float, initial_soc: float = 1.0):
-        self.C_nom = capacity_nom_Ah * 3600.0  # Kapazität in As
-        self.soc = initial_soc
-        self.R_int = 0.08
-        self.Vmin = 32.0
-        self.Vmax = 42.0
 
     @abstractmethod
-    def apply_current(self, current: float, duration: float) -> None:
+    def __init__(self, capacity_nom_Ah: float, initial_soc: float = 1.0):
+        pass
+
+    @abstractmethod
+    def reset_soc(self) -> float:
+        pass
+
+    @abstractmethod
+    def apply_current(self, current: float, duration: float) -> float:
+        pass
+
+    @abstractmethod
+    def is_empty(self) -> bool:
+        pass
+
+    @abstractmethod
+    def is_full(self) -> bool:
         pass
 
     @abstractmethod
     def voltage(self, current: float = 0.0) -> float:
+        pass
+
+    @abstractmethod
+    def power(self, current: float = 0.0) -> float:
         pass
