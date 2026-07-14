72c57dddf31ac2391ff993e0ab56ce59fd49abe4
Horstl123 <ha9358@mci4me.at>
2026-07-14 11:02:07 +0200

    feat: add incline calculation

--- src/route.py
+++ src/route.py
@@ -1,19 +1,15 @@
 import pandas as pd
-import numpy as np  # Neu: Für die mathematischen Berechnungen (Sinus, Cosinus etc.)
+import numpy as np 
 
 class RouteAnalysis:
     
     def __init__(self, dateipfad):
         self.daten = pd.read_csv(dateipfad, sep=';')
-        
-       
         self.daten['time'] = pd.to_datetime(self.daten['time'])
         
     def geschwindigkeit(self):
-        
         self.daten['delta_t_sekunden'] = self.daten['time'].diff().dt.total_seconds()
         
-
         lat_vorher = self.daten['lat'].shift(1)
         lon_vorher = self.daten['lon'].shift(1)
         lat_jetzt = self.daten['lat']
@@ -41,30 +37,24 @@ class RouteAnalysis:
         self.daten['geschwindigkeit_km_h'] = self.daten['geschwindigkeit_m_s'] * 3.6
         
     def beschleunigung(self):
-        print("Beschleunigung:")
-        
         delta_v = self.daten['geschwindigkeit_m_s'].diff()
-        
-        # Beschleunigung 
         self.daten['beschleunigung_m_s2'] = delta_v / self.daten['delta_t_sekunden']
         
+    def steigung(self):
+        delta_h = self.daten['ele'].diff()
+        
+        strecke = self.daten['delta_s_meter'].replace(0, np.nan)
+        
+        self.daten['steigung_prozent'] = (delta_h / strecke) * 100
+        self.daten['steigung_winkel_rad'] = np.arctan(delta_h / strecke)
 
 
 
 meine_fahrt = RouteAnalysis('simulation_data/final_project_input_data.csv')
 
 meine_fahrt.geschwindigkeit()
-
-print(meine_fahrt.daten[['time', 'delta_t_sekunden', 'delta_s_meter', 'geschwindigkeit_km_h']].head())
-
-
-meine_fahrt = RouteAnalysis('simulation_data/final_project_input_data.csv')
-
-# Geschwindigkeit
-meine_fahrt.geschwindigkeit()
-
-# Beschleunigung 
 meine_fahrt.beschleunigung()
+meine_fahrt.steigung()
 
 
-print(meine_fahrt.daten[['time', 'geschwindigkeit_km_h', 'beschleunigung_m_s2']].head())
+print(meine_fahrt.daten[['time', 'geschwindigkeit_km_h', 'beschleunigung_m_s2', 'steigung_prozent']].head())
